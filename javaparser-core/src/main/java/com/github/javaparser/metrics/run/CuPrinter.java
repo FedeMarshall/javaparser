2c0aa8a24de9bf13cfc819b066238603e20b9203
Federico Canet <fedeea@gmail.com>
2016-11-09 20:36:48 -0300

    Revert "9-11"
    This reverts commit 6fea5123295afc8af72bce2eedf1b5a03ea2cbd7.

--- javaparser-core/src/main/java/com/github/javaparser/metrics/run/CuPrinter.java
+++ javaparser-core/src/main/java/com/github/javaparser/metrics/run/CuPrinter.java
@@ -1,12 +1,5 @@
 package com.github.javaparser.metrics.run;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
@@ -16,12 +9,9 @@ import com.github.javaparser.metrics.utilities.HalsteadComplexityMeasures;
 
 
 public class CuPrinter {
-	
-	final static String originPath = "src/main/java/com/github/javaparser";
 
     public static void main(String[] args) throws Exception {
-        /*
-    	// creates an input stream for the file to be parsed
+        // creates an input stream for the file to be parsed
         FileInputStream in = new FileInputStream("Test.java");
 
         CompilationUnit cu;
@@ -59,51 +49,6 @@ public class CuPrinter {
 		halsteadMetric.setDifficulty(halsteadMeasures.difficulty());
 		halsteadMetric.setEffort(halsteadMeasures.effort());
 		
-		ExecuteGeneratorOutputHtmlFile.execute(halsteadMetric);    
-		*/
-		executeGenOutputHtmoFile();
+		ExecuteGeneratorOutputHtmlFile.execute(halsteadMetric);        
     }
-    
-    public static void executeGenOutputHtmoFile(){
-		File d1 = new File(originPath);
-		enterDirectorios(d1);
-	}
-
-	public static void enterDirectorios(File d1){
-
-		if (d1.isDirectory()){
-			// Recorremos recursivamente el directorio
-			String[] ficheros = d1.list();
-			for (int x=0;x<ficheros.length;x++) {
-				enterDirectorios(new File(d1,ficheros[x]));
-			}
-		}else{
-		    // ejecutamos, ya que serán ficheros
-			ejecutarJavaParserMetrics(d1);
-		}
-	}
-
-	public static void ejecutarJavaParserMetrics(File f1){
-
-		try {
-			//FileInputStream in = new FileInputStream("Test.java");
-			//InputStream in = new FileInputStream(f1);
-	        FileInputStream in = new FileInputStream(f1);
-	        
-	        CompilationUnit cu;
-	        try {
-	            // parse the file
-	            cu = JavaParser.parse(in);
-	        } finally {
-	            in.close();
-	        }
-
-	        // prints the resulting compilation unit to default system output
-	        System.out.println(cu.toString());
-
-
-		} catch (IOException ioe){
-			ioe.printStackTrace();
-		}
-	}
 }
